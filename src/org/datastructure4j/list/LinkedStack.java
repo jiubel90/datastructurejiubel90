dbd0d298bae5cf0c099a716beda83cb7b8caacc1
David Ricardo <dricardo.ledob@gmail.com>
2021-05-25 07:44:17 -0700

    Renaming using naming conventions.

---
+++ src/org/datastructure4j/list/LinkedStack.java
@@ -0,0 +1,38 @@
+package org.datastructure4j.list;
+
+public class LinkedStack<E> implements Stack<E> {
+	
+	private SingleLinkedList<E> list = new SingleLinkedList<>(); // an empty list
+	
+	public LinkedStack() { } // new stack relies on the initially empty list
+	
+	public int size() { 
+		return list.size(); 
+	}
+	
+	public boolean isEmpty() { 
+		return list.isEmpty(); 
+	}
+	
+	public void push(E element) { 
+		list.addFirst(element); 
+	}
+	
+	public E top() { 
+		return list.first();
+	}
+	
+	public E pop() {
+		return list.removeFirst(); 
+	}
+	
+	/** A generic method for reversing an array. */
+	public static <E> void reverse(E[] a) {
+			Stack<E> buffer = new ArrayStack<>(a.length);
+			for (int i=0; i < a.length; i++)
+			buffer.push(a[i]);
+			for (int i=0; i < a.length; i++)
+			a[i] = buffer.pop();
+	}
+	
+}
