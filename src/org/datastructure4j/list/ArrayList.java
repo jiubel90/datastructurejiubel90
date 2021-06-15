07a06d492ed029323affeefaf9980e2b26ac9870
RobertoGP96 <trolbertogp96@gmail.com>
2021-06-15 18:12:18 -0400

    add implementation and related methods

--- src/org/datastructure4j/list/ArrayList.java
+++ src/org/datastructure4j/list/ArrayList.java
@@ -1,5 +1,6 @@
 package org.datastructure4j.list;
 
+import java.util.Arrays;
 import java.util.Iterator;
 
 public class ArrayList<E> implements List<E> {
@@ -47,7 +48,7 @@ public class ArrayList<E> implements List<E> {
 	/**
 	 * Inserts element e to be at index i, shifting all subsequent elements later.
 	 */
-	public void add(int i, E e) throws IndexOutOfBoundsException, IllegalStateException {
+	public void insert(int i, E e) throws IndexOutOfBoundsException, IllegalStateException {
 		checkIndex(i, size + 1);
 		if (size == data.length) // not enough capacity
 			throw new IllegalStateException("Array is full");
@@ -96,17 +97,90 @@ public class ArrayList<E> implements List<E> {
 			}
 		};
 	}
-
+	/**
+	 * Add the element at the end of array 
+	 * */
 	@Override
 	public void add(E e) throws IndexOutOfBoundsException {
-		// TODO Auto-generated method stub
-		
+		ensureCapacityInternal(size + 1); 
+		data[this.size++]=e;
 	}
+	
+	/**
+	 * 
+	 * */
+	
+	 /**
+     * Increases the capacity of this <tt>ArrayList</tt> instance, if
+     * necessary, to ensure that it can hold at least the number of elements
+     * specified by the minimum capacity argument.
+     *
+     * @param   minCapacity   the desired minimum capacity
+     */
 
-	@Override
-	public void insert(int i, E e) throws IndexOutOfBoundsException {
-		// TODO Auto-generated method stub
-		
-	}
+    /**
+     * Shared empty array instance used for empty instances.
+     */
+    private static final Object[] EMPTY_DATA = {};
 
+    public void ensureCapacity(int minCapacity) {
+        int minExpand = (data != EMPTY_DATA)
+            // any size if real element table
+            ? 0
+            // larger than default for empty table. It's already supposed to be
+            // at default size.
+            : CAPACITY;
+
+        if (minCapacity > minExpand) {
+            ensureExplicitCapacity(minCapacity);
+        }
+    }
+
+    private void ensureCapacityInternal(int minCapacity) {
+        if (data == EMPTY_DATA) {
+            minCapacity = Math.max(CAPACITY, minCapacity);
+        }
+
+        ensureExplicitCapacity(minCapacity);
+    }
+
+    private void ensureExplicitCapacity(int minCapacity) {
+       // overflow-conscious code
+        if (minCapacity - data.length > 0)
+            grow(minCapacity);
+    }
+
+    /**
+     * The maximum size of array to allocate.
+     * Some VMs reserve some header words in an array.
+     * Attempts to allocate larger arrays may result in
+     * OutOfMemoryError: Requested array size exceeds VM limit
+     */
+    private static final int MAX_ARRAY_SIZE = Integer.MAX_VALUE - 8;
+
+    /**
+     * Increases the capacity to ensure that it can hold at least the
+     * number of elements specified by the minimum capacity argument.
+     *
+     * @param minCapacity the desired minimum capacity
+     */
+    private void grow(int minCapacity) {
+        // overflow-conscious code
+        int oldCapacity = data.length;
+        int newCapacity = oldCapacity + (oldCapacity >> 1);
+        if (newCapacity - minCapacity < 0)
+            newCapacity = minCapacity;
+        if (newCapacity - MAX_ARRAY_SIZE > 0)
+            newCapacity = hugeCapacity(minCapacity);
+        // minCapacity is usually close to size, so this is a win:
+        data = Arrays.copyOf(data, newCapacity);
+    }
+
+    private static int hugeCapacity(int minCapacity) {
+        if (minCapacity < 0) // overflow
+            throw new OutOfMemoryError();
+        return (minCapacity > MAX_ARRAY_SIZE) ?
+            Integer.MAX_VALUE :
+            MAX_ARRAY_SIZE;
+    }
 }
